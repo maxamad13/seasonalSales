e83342625d72f0e69c58e42e2cefcfb456b3bd20
Allen Phelps <allenatmc@gmail.com>
2015-09-19 13:15:12 -0500

    add in json files, clean up entry.js

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -4,19 +4,13 @@ requirejs.config({
 'jquery': '../lib/bower_components/jquery/dist/jquery.min',
 'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
 'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-'firebase': '../lib/bower_components/firebase/firebase',
 'lodash': '../lib/bower_components/lodash/lodash.min',
-'q': '../lib/bower_components/q/q'
-
 	},
 
 'shim': {
-        'bootstrap': ['jquery'],
-        'firebase': {
-        		exports: 'Firebase'
-        }
+        'bootstrap': ['jquery']
   }
 
 });
 
-require(['jquery','hbs','bootstrap', 'firebase', 'lodash','main'], function($, Handlebars, bootstrap, fb, _, main) {} );
+require(['jquery','hbs','bootstrap', 'lodash'], function($, Handlebars, bootstrap, _) {} );
